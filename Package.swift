d11e0de55145750d316aa0f4f4f35ac2e7152fe5
Ian Carbone <ian@affinitiquest.io>
2025-08-27 15:18:45 -0400

    Update version.

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["SpruceIDMobileSdk"])
     ],
     dependencies: [
-        .package(url: "https://github.com/affinitiquest/mobile-sdk-rs", from: "1.0.2"),
+        .package(url: "https://github.com/affinitiquest/mobile-sdk-rs", from: "1.0.3"),
         .package(url: "https://github.com/apple/swift-algorithms", from: "1.2.0")
     ],
     targets: [
